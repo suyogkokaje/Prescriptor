a71688cec0cd70ddd7399da5553364ca514750a8
suyogkokaje <suyogkokaje6@gmail.com>
2023-05-14 15:51:14 +0530

    feat:added delete history feature

--- src/pages/PatientHistory.jsx
+++ src/pages/PatientHistory.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 import "./patienthistory.css";
 
@@ -31,12 +31,30 @@ const PatientPage = () => {
           </div>
         ))
       : null;
-
+  const navigate = useNavigate();
+  const deletePatientHistory = async () => {
+    try {
+      await axios.delete(`http://localhost:4000/patients/${name}`);
+      setPatientHistory(null);
+    } catch (error) {
+      console.error(error);
+    }
+    navigate("/patients");
+  };
   return (
     <div className="patient-card-parent">
       {patientHistory && (
         <div className="patient-card">
           <div className="patient-card-heading">Patient History</div>
+          <div className="btn-div">
+            <button className="btn" onClick={() => navigate("/")}>
+              Create Prescription
+            </button>
+            <button className="btn">Edit History</button>
+            <button className="btn" onClick={deletePatientHistory}>
+              Delete History
+            </button>
+          </div>
           <p>
             <span>Name</span>: {patientHistory.name}
           </p>
@@ -79,7 +97,8 @@ const PatientPage = () => {
             <div className="card-options">{optionsList}</div>
           </div>
           <p>
-            <span>Menstrual/Obstetrical History</span>: {patientHistory.menshistory}
+            <span>Menstrual/Obstetrical History</span>:{" "}
+            {patientHistory.menshistory}
           </p>
         </div>
       )}
