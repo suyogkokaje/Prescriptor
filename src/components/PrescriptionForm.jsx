d3ba2fac88e9436a51654645a2e1acffe89c6a4f
suyogkokaje <suyogkokaje6@gmail.com>
2023-07-05 07:43:29 +0530

    fix: fixed form autofill

--- src/components/PrescriptionForm.jsx
+++ src/components/PrescriptionForm.jsx
@@ -21,41 +21,52 @@ const PrescriptionForm = ({ handleAddItem, handleSetPatientName }) => {
       frequency: "Twice daily",
       duration: "10 days",
       totalQuantity: "20 tablets",
-      instructions: "Take with water after meals."
+      instructions: "Take with water after meals.",
     },
     {
-      medicine: "chandraprabha vati",
+      medicine: "cmandraprabha vati",
       preparation: "Tab",
       dose: "1 tablet",
       direction: "After meal",
       frequency: "Twice daily",
       duration: "10 days",
       totalQuantity: "20 tablets",
-      instructions: "Take with water after meals."
-    }
-  ]
+      instructions: "Take with water after meals.",
+    },
+    {
+      medicine: "amritarishta",
+      preparation: "Tab",
+      dose: "1 tablet",
+      direction: "After meal",
+      frequency: "Twice daily",
+      duration: "10 days",
+      totalQuantity: "20 tablets",
+      instructions: "Take with water after meals.",
+    },
+  ];
 
   const [medicine, setMedicine] = useState("");
   const [preperation, setPreperation] = useState("");
-  const [dose,setDose] = useState("");
-  const [direction,setDirection] = useState("");
-  const [frequency,setFrequency] = useState("");
-  const [duration,setDuration] = useState("");
-  const [totalQuantity,setTotalQuantity] = useState("");
+  const [dose, setDose] = useState("");
+  const [direction, setDirection] = useState("");
+  const [frequency, setFrequency] = useState("");
+  const [duration, setDuration] = useState("");
+  const [totalQuantity, setTotalQuantity] = useState("");
   const [instructions, setInstructions] = useState("");
   const [lst, setLst] = useState([]);
-  const [medicineSuggestions, setMedicineSuggestions] = useState([]);
+  const [selectedSuggestion, setSelectedSuggestion] = useState("");
+
 
   const handleAdd = (event) => {
     event.preventDefault(); // prevent form submission
     const prescription = {
       medicine: medicine,
       preperation: preperation,
-      dose:dose,
-      direction:direction,
-      frequency:frequency,
-      duration:duration,
-      totalQuantity:totalQuantity,
+      dose: dose,
+      direction: direction,
+      frequency: frequency,
+      duration: duration,
+      totalQuantity: totalQuantity,
       instructions: instructions,
     };
     handleAddItem(prescription);
@@ -97,25 +108,12 @@ const PrescriptionForm = ({ handleAddItem, handleSetPatientName }) => {
       setTotalQuantity("");
       setInstructions("");
       setLst([]);
-      console.log("This is the data:" + response.data); // log the response data
     } catch (error) {
       console.error(error); // log any errors
     }
   };
 
-  const handleMedicineChange = (event) => {
-    const value = event.target.value;
-    setMedicine(value);
-    // Filter medicine suggestions based on input value
-    const filteredSuggestions = medicineList.filter((item) =>
-      item.medicine.toLowerCase().includes(value.toLowerCase())
-    );
-    setMedicineSuggestions(filteredSuggestions);
-    
-    handleMedicineSuggestionClick(filteredSuggestions[0]);
-  };
-
-  const handleMedicineSuggestionClick = (suggestion) => {
+  const handleSelectSuggestion = (suggestion) => {
     setMedicine(suggestion.medicine);
     setPreperation(suggestion.preparation);
     setDose(suggestion.dose);
@@ -124,8 +122,9 @@ const PrescriptionForm = ({ handleAddItem, handleSetPatientName }) => {
     setDuration(suggestion.duration);
     setTotalQuantity(suggestion.totalQuantity);
     setInstructions(suggestion.instructions);
-    setMedicineSuggestions([]);
+    setSelectedSuggestion(suggestion.medicine);
   };
+  
 
   return (
     <div className="form-parent">
@@ -147,33 +146,45 @@ const PrescriptionForm = ({ handleAddItem, handleSetPatientName }) => {
           <br></br>
           <div className="form-tile">
             <label>Name of the medicine:</label>
-            <input
-              value={medicine}
-              onChange={handleMedicineChange}
-              type="text"
-              list="medicineSuggestions"
-            />
+            <div className="suggestions">
+              <input
+                value={medicine}
+                onChange={(e) => setMedicine(e.target.value)}
+              />
+              {medicine &&
+                medicineList
+                  .filter(
+                    (item) =>
+                      item.medicine
+                        .toLowerCase()
+                        .indexOf(medicine.toLowerCase()) !== -1
+                  )
+                  .map((suggestion) => (
+                    <button
+                      key={suggestion.medicine}
+                      onClick={() => handleSelectSuggestion(suggestion)}
+                      className={`suggestion ${
+                        selectedSuggestion === suggestion.medicine
+                          ? "selected"
+                          : ""
+                      }`}
+                    >
+                      {suggestion.medicine}
+                    </button>
+                  ))}
+            </div>
           </div>
-          <datalist id="medicineSuggestions">
-              {medicineSuggestions.map((suggestion, index) => (
-                <option
-                  key={index}
-                  value={suggestion.medicine}
-                  onClick={() => handleMedicineSuggestionClick(suggestion)}
-                />
-              ))}
-            </datalist>
           <div className="form-tile">
             <label>Preparation:</label>
-              <select
-                value={preperation}
-                onChange={(e) => setPreperation(e.target.value)}
-              >
-                <option value="">Select an Option</option>
-                <option value="Tab">Tab</option>
-                <option value="Cap">Cap</option>
-                <option value="Inj">Inj</option>
-              </select>
+            <select
+              value={preperation}
+              onChange={(e) => setPreperation(e.target.value)}
+            >
+              <option value="">Select an Option</option>
+              <option value="Tab">Tab</option>
+              <option value="Cap">Cap</option>
+              <option value="Inj">Inj</option>
+            </select>
           </div>
           <div className="form-tile">
             <label>Duration:</label>
@@ -201,42 +212,44 @@ const PrescriptionForm = ({ handleAddItem, handleSetPatientName }) => {
           </div>
           <div className="form-tile">
             <label>Direction:</label>
-              <select
-                value={direction}
-                onChange={(e) => setDirection(e.target.value)}
-              >
-                <option value="">Select an Option</option>
-                <option value="Before breakfast">Before breakfast</option>
-                <option value="After breakfast">After breakfast</option>
-                <option value="Before lunch">Before lunch</option>
-                <option value="After lunch">After lunch</option>
-                <option value="Before dinner">Before dinner</option>
-                <option value="After dinner">After dinner</option>
-                <option value="Before meal">Before meal</option>
-                <option value="After meal">After meal</option>
-              </select>
+            <select
+              value={direction}
+              onChange={(e) => setDirection(e.target.value)}
+            >
+              <option value="">Select an Option</option>
+              <option value="Before breakfast">Before breakfast</option>
+              <option value="After breakfast">After breakfast</option>
+              <option value="Before lunch">Before lunch</option>
+              <option value="After lunch">After lunch</option>
+              <option value="Before dinner">Before dinner</option>
+              <option value="After dinner">After dinner</option>
+              <option value="Before meal">Before meal</option>
+              <option value="After meal">After meal</option>
+            </select>
           </div>
           <div className="form-tile">
             <label>Frequency:</label>
-              <select
-                value={frequency}
-                onChange={(e) => setFrequency(e.target.value)}
-              >
-                <option value="">Select an Option</option>
-                <option value="If required">If required</option>
-                <option value="Immediately">Immediately</option>
-                <option value="Once a day">Once a day</option>
-                <option value="Twice daily">Twice daily</option>
-                <option value="Thrice daily">Thrice daily</option>
-                <option value="Four times a day">Four times a day</option>
-                <option value="Every hour">Every hour</option>
-                <option value="Every night at bedtime">Every night at bedtime</option>
-                <option value="Every day">Every day</option>
-                <option value="Every other day">Every other day</option>
-                <option value="Every four hours">Every four hours</option>
-                <option value="Once a week">Once a week</option>
-                <option value="Thrice a week">Thrice a week</option>
-              </select>
+            <select
+              value={frequency}
+              onChange={(e) => setFrequency(e.target.value)}
+            >
+              <option value="">Select an Option</option>
+              <option value="If required">If required</option>
+              <option value="Immediately">Immediately</option>
+              <option value="Once a day">Once a day</option>
+              <option value="Twice daily">Twice daily</option>
+              <option value="Thrice daily">Thrice daily</option>
+              <option value="Four times a day">Four times a day</option>
+              <option value="Every hour">Every hour</option>
+              <option value="Every night at bedtime">
+                Every night at bedtime
+              </option>
+              <option value="Every day">Every day</option>
+              <option value="Every other day">Every other day</option>
+              <option value="Every four hours">Every four hours</option>
+              <option value="Once a week">Once a week</option>
+              <option value="Thrice a week">Thrice a week</option>
+            </select>
           </div>
           <div className="form-tile">
             <label>Instructions:</label>
